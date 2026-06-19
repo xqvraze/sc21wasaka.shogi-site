b4e6b9789b074810b02c78572b3827570e17e0b5
xqvraze <sc21wasaka.shogi@gmail.com>
2026-06-19 17:40:46 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -19,11 +19,11 @@ function goPage(page){
 
 function change_text(){
 	//お知らせ内容
-	var msgNewInfo = 'お知らせ 2026/6/12現在';
+	var msgNewInfo = 'お知らせ 2026/6/19現在';
 	document.getElementById('newInfo').innerHTML = msgNewInfo;
 
 	//当面の活動日
-	var msgCalender = '<p>当面の活動日 6/13(土)、6/20(土)、7/11(土)、7/18(土)、8/8(土)、8/22(土)<br>※参考情報<br>7/4(土) 指導者都合<br>7/21(火) 1学期終業式<br>7/26(土) 和坂納涼祭<br>8/1(土)指導者都合<br>8/28(金)                                                                                                                                                                                                                                                                                                                                                                                                                   2学期始業式</p>';
+	var msgCalender = '<p>当面の活動日 6/20(土)、7/11(土)、7/18(土)、8/8(土)、8/22(土)<br>※参考情報<br>7/4(土) 指導者都合<br>7/21(火) 1学期終業式<br>7/26(土) 和坂納涼祭<br>8/1(土)指導者都合<br>8/28(金)                                                                                                                                                                                                                                                                                                                                                                                                                   2学期始業式</p>';
 document.getElementById('dayOfAct').innerHTML = msgCalender;
 
 }
