32fa576dc303bc46571043d9c5b32635215250a8
xqvraze <sc21wasaka.shogi@gmail.com>
2026-09-04 17:51:43 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -19,11 +19,11 @@ function goPage(page){
 
 function change_text(){
 	//お知らせ内容
-	var msgNewInfo = 'お知らせ 2026/8/9現在';
+	var msgNewInfo = 'お知らせ 2026/9/4現在';
 	document.getElementById('newInfo').innerHTML = msgNewInfo;
 
 	//当面の活動日
-	var msgCalender = '<p>当面の活動日 8/8(土)、8/22(土)、9/5(土)、9/19(土)、10/17(土)、10/31(土)<br>※参考情報<br>8/28(金)                                                                                                                                                                                                                                                                                                                                                                                                                   2学期始業式<br>10/3(土) 野々池中学校体育大会<br>10/10(土) 和坂校区健康診断<br>10/24(土) 和坂小学校体育大会</p>';
+	var msgCalender = '<p>当面の活動日 9/5(土)、9/19(土)、10/17(土)、10/31(土)、11/14(土)、11/28(土)<br>※参考情報<br>10/3(土) 野々池中学校 体育大会<br>10/10(土) 和坂校区健康診断<br>10/24(土) 和坂小学校 体育大会<br>11/7(土) 和坂校区健康診断<br>11/21(土) 和坂小学校 音楽会<br>11/22(土) 地域探検ウオークラリー大会</p>';
 document.getElementById('dayOfAct').innerHTML = msgCalender;
 
 }
